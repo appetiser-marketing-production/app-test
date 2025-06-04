41759fc0c9f596a532102335bf2e396c961a08b1
Norbert Feria <norbert.feria@gmail.com>
2025-06-04 13:04:20 +0800

    test 15

--- app-test.php
+++ app-test.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: app-test
  * Plugin URI:  https://appetiser.com.au
- * Description: app-test trigger test 14
+ * Description: app-test trigger test 15
  * Version: 1.0.0
  * Author: Landing page team
  * Author URI: https://appetiser.com.au
